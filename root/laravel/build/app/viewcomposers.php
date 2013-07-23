d29658508e52263fce58043d1ef8a867165c75ae
Pete Feltham <peter@feltham.com.au>
2013-07-23 17:24:14 +1000

    Add Laravel view composers file

---
+++ root/laravel/build/app/viewcomposers.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| View Composers
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the view composers and shared view
+| data for your application.
+|
+*/
+
+//App::before(function($request) {
+//    // make a piece of model data available in all views
+//    View::share('key', 'value');
+//});
+
+//View::composer('layouts.common', function($view) {
+//    $view->with(array(
+//        'key' => 'value',
+//    ));
+//});
