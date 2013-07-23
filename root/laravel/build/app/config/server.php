bda6552fa64d1b5008443f46a12988ae3a1ee09c
Pete Feltham <peter@feltham.com.au>
2013-07-23 16:40:06 +1000

    Refactor laravel template code into multiple js files. Add support for configuring Laravel with trusted proxies.

---
+++ root/laravel/build/app/config/server.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+    /*
+    |--------------------------------------------------------------------------
+    | Reverse proxy configuration
+    |--------------------------------------------------------------------------
+    |
+    | If the server is behind a reverse proxy, configure its IP address here so
+    | that Laravel considers the headers sent by the proxy when considering the
+    | request.
+    |
+    | For example, the proxy may pass a X-Forwarded-Proto header to indicate
+    | that the client was using https.
+    |
+    | Each item in the array should be either a plain IP address such as
+    | '127.0.0.1', or a subnet such as '10.133.0.0/24'.
+    |
+    */
+
+    'trusted_proxies' => array(
+    ),
+);
